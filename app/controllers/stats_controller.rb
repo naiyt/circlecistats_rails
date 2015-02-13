e8b75345bf9cb2a21ea657783ed2ea52e2d18f22
Nate Collings <nate@natecollings.com>
2015-02-13 15:01:04 -0700

    Print highest build

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,4 +1,34 @@
 class StatsController < ApplicationController
+  before_action :set_builds
+  before_action :set_stats
+
   def index
+  end
+
+  private
+
+  def set_builds
+    @builds = Build.all
+  end
+
+  def set_stats
+    @average = average
+    @maximum = maximum
+    p @maximum
+  end
+
+  # TODO - Rails generator in CircleCIStats gem to create the model, allow these to be computed inside the gem?
+  def average(builds=nil)
+    builds = Build.with_times if builds.nil?
+    times = builds.map { |build| build.to_seconds }
+    (times.inject(:+) / times.length).round(2)
+  end
+
+  def maximum
+    Build.ordered_by_time.first
+  end
+
+  def to_seconds(ms)
+    ms / 1000 / 60
   end
 end
--- app/models/build.rb
+++ app/models/build.rb
@@ -1,2 +1,9 @@
 class Build < ActiveRecord::Base
+
+  scope :with_times, lambda { where("build_time IS NOT NULL") }
+  scope :ordered_by_time, lambda { order("build_time DESC") }
+
+  def to_seconds
+    (build_time / 1000.0 / 60).round(2)
+  end
 end
